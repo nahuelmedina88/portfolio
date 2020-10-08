64f0870952d45047baf41f35899f41df59b83ac8
nahuelmedina88 <nahuelmedinasenn@gmail.com>
2020-10-08 21:03:23 +0200

    third deploy my portfolio

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 
+
+
 library.add(fab, fas);
 
 //import Flexbox from "./componentes/flexbox";
--- src/componentes/main.js
+++ src/componentes/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Contact from "./Contact";
 import Aboutme from "./Aboutme";
@@ -7,13 +7,21 @@ import MyProjects from "./MyProjects";
 import Resume from "./Resume/Resume";
 
 const Main = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/contact" component={Contact} />
-    <Route exact path="/aboutme" component={Aboutme} />
-    <Route exact path="/myprojects" component={MyProjects} />
-    <Route exact path="/resume" component={Resume} />
-  </Switch>
+  <HashRouter basename='/'>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/aboutme" component={Aboutme} />
+      <Route exact path="/myprojects" component={MyProjects} />
+      <Route exact path="/resume" component={Resume} />
+    </Switch>
+  </HashRouter>
 );
+
+// const Home = () => { <div><h2>Home</h2></div> }
+// const Contact = () => <div><h2>Contact</h2></div>
+// const AboutMe = () => <div><h2>AboutMe</h2></div>
+// const MyProjects = () => <div><h2>MyProjects</h2></div>
+// const Resume = () => <div><h2>Resume</h2></div>
 
 export default Main;
