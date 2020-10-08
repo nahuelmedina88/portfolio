58c4d22bb4723b557e330ecedce4a7e200c0cacd
nahuelmedina88 <nahuelmedinasenn@gmail.com>
2020-10-08 18:18:58 +0200

    Updates

---
+++ precache-manifest.b0ce8e0882be61ea2318fc9a392cef12.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42eeb72726edb82ffe276a8968679ec0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b9c3db6188e0ab0cd8ab",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "327f6018e119df26892b",
+    "url": "/portfolio/static/css/main.2a0456b2.chunk.css"
+  },
+  {
+    "revision": "b9c3db6188e0ab0cd8ab",
+    "url": "/portfolio/static/js/2.12292827.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/portfolio/static/js/2.12292827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327f6018e119df26892b",
+    "url": "/portfolio/static/js/main.bb06d07f.chunk.js"
+  },
+  {
+    "revision": "5e8cfc1d2358202fbc47",
+    "url": "/portfolio/static/js/runtime-main.b958c5cb.js"
+  },
+  {
+    "revision": "97507903ed9b1f247ccf2569d23ae330",
+    "url": "/portfolio/static/media/boston3.97507903.jpg"
+  },
+  {
+    "revision": "7c20b6029ed11a85b81516a9d3a6c8e1",
+    "url": "/portfolio/static/media/kohlanta.7c20b602.JPG"
+  },
+  {
+    "revision": "4609912d51c856fce8bf7f9c0609ac46",
+    "url": "/portfolio/static/media/losotrodo.4609912d.jpg"
+  },
+  {
+    "revision": "0b22a7ee1717969533a1f8d0a6d30d82",
+    "url": "/portfolio/static/media/nahue2020Zoom.0b22a7ee.jpg"
+  },
+  {
+    "revision": "2c5ef616476b2fec11ea2bb83a5cc9cb",
+    "url": "/portfolio/static/media/nahuel2016.2c5ef616.png"
+  },
+  {
+    "revision": "432c46af2ae6e2e40a4a0207f7006533",
+    "url": "/portfolio/static/media/sindicato.432c46af.jpg"
+  }
+]);
