a0302a16a5d16aeed6aa154f2e1060539f8f2a76
nahuelmedina88 <nahuelmedinasenn@gmail.com>
2020-10-09 19:09:53 +0200

    testing route for github pages

--- src/componentes/MenuNavigation.js
+++ src/componentes/MenuNavigation.js
@@ -10,31 +10,28 @@ export default class MenuNavigation extends Component {
       <Fragment>
         <Navbar className={styles.menu} bg="dark" variant="dark" expand="lg">
           <Navbar.Brand
-            className={`${
-              this.props.route === "/" ? styles.brand : styles.brand2
-            }`}
+            className={`${this.props.route === "/" ? styles.brand : styles.brand2
+              }`}
             href="/"
           >
             Nahuel Medina Senn
           </Navbar.Brand>
           <Navbar.Toggle
-            className={`${
-              this.props.route === "/" ? styles.toggle : styles.toggle2
-            }`}
+            className={`${this.props.route === "/" ? styles.toggle : styles.toggle2
+              }`}
             aria-controls="basic-navbar-nav"
           />
           <Navbar.Collapse
-            className={`${
-              this.props.route === "/" ? styles.collapse : styles.collapse2
-            }`}
+            className={`${this.props.route === "/" ? styles.collapse : styles.collapse2
+              }`}
             id="basic-navbar-nav"
           >
             <Nav className="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/aboutme">About Me</Nav.Link>
-              <Nav.Link href="/myprojects">My Projects</Nav.Link>
-              <Nav.Link href="/resume">Resume</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="/portfolio/#">Home</Nav.Link>
+              <Nav.Link href="/portfolio/#/aboutme">About Me</Nav.Link>
+              <Nav.Link href="/portfolio/#/myprojects">My Projects</Nav.Link>
+              <Nav.Link href="/portfolio/#/resume">Resume</Nav.Link>
+              <Nav.Link href="/portfolio/#/contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
