1f25fe2a386b612b8324e83c1a14dd3a0e4f5b17
nahuelmedina88 <nahuelmedinasenn@gmail.com>
2020-10-09 18:57:42 +0200

    testing route for github pages

--- src/componentes/main.js
+++ src/componentes/main.js
@@ -7,21 +7,14 @@ import MyProjects from "./MyProjects";
 import Resume from "./Resume/Resume";
 
 const Main = () => (
-  <HashRouter basename='/'>
+  <HashRouter basename='/portfolio'>
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/portfolio" component={Home} />
       <Route exact path="/contact" component={Contact} />
-      <Route exact path="/aboutme" component={Aboutme} />
+      <Route path="/aboutme" component={Aboutme} />
       <Route exact path="/myprojects" component={MyProjects} />
       <Route exact path="/resume" component={Resume} />
     </Switch>
   </HashRouter>
 );
-
-// const Home = () => { <div><h2>Home</h2></div> }
-// const Contact = () => <div><h2>Contact</h2></div>
-// const AboutMe = () => <div><h2>AboutMe</h2></div>
-// const MyProjects = () => <div><h2>MyProjects</h2></div>
-// const Resume = () => <div><h2>Resume</h2></div>
-
 export default Main;
